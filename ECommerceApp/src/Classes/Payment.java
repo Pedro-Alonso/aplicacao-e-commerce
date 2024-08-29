3c3bad972aa7bf12f689df0e209de05f2fa910f6
Pedro Alonso <pedro.alonsodeoliveira@hotmail.com>
2024-08-29 15:05:53 -0300

    feat: getters and setters added

--- ECommerceApp/src/Classes/Payment.java
+++ ECommerceApp/src/Classes/Payment.java
@@ -18,4 +18,70 @@ public class Payment {
         PAID,
         CANCELLED
     }
+
+    public UUID getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(UUID paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDateTime getIssueDate() {
+        return issueDate;
+    }
+
+    public void setIssueDate(LocalDateTime issueDate) {
+        this.issueDate = issueDate;
+    }
+
+    public LocalDateTime getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDateTime dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public LocalDateTime getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(LocalDateTime paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public Status getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(Status paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+    public ECommerceUser getSender() {
+        return sender;
+    }
+
+    public void setSender(ECommerceUser sender) {
+        this.sender = sender;
+    }
+
+    public ECommerceUser getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(ECommerceUser receiver) {
+        this.receiver = receiver;
+    }
+
+    
 }
