4116f373280f11826a01385ce754205f488e670a
Pedro Alonso <pedro.alonsodeoliveira@hotmail.com>
2024-09-05 15:08:36 -0300

    docs: improved methods documentation

--- ECommerceApp/src/Classes/Payment.java
+++ ECommerceApp/src/Classes/Payment.java
@@ -20,6 +20,13 @@ public class Payment {
     CANCELLED,
   }
 
+  /**
+   * Constructor for the Payment class
+   * @param amount The amount of the payment (in BRL) -> {@code double}
+   * @param dueDate The due date of the payment -> {@link LocalDateTime}
+   * @param sender The sender of the payment -> {@link ECommerceUser}
+   * @param receiver The receiver of the payment -> {@link ECommerceUser}
+   */
   public Payment(
     double amount,
     LocalDateTime dueDate,
@@ -37,8 +44,7 @@ public class Payment {
   }
 
   /**
-   * Method that returns the attributes of the Payment object
-   * @return void
+   * {@return the attributes of the Payment object as a {@link String}}
    */
   public String generateReceipt() {
     return (
@@ -66,8 +72,7 @@ public class Payment {
   }
 
   /**
-   * Method to set the Payment as PAID
-   * @return void
+   * Method to set the Payment as {@link PaymentStatus#PAID}
    */
   public void pay() {
     this.paymentDate = LocalDateTime.now();
@@ -75,70 +80,121 @@ public class Payment {
   }
 
   /**
-   * Method to cancel the Payment
-   * @return void
+   * Method to set the Payment as {@link PaymentStatus#CANCELLED}
    */
   public void cancel() {
     this.paymentDate = null;
     this.paymentStatus = PaymentStatus.CANCELLED;
   }
 
+  /**
+   * {@return the paymentId as a {@link UUID}}
+   */
   public UUID getPaymentId() {
     return paymentId;
   }
 
+  /**
+   * {@return the amount of the payment (in BRL) as a {@code double}}
+   */
   public double getAmount() {
     return amount;
   }
 
+  /**
+   * Setter for the amount attribute
+   * @param amount The amount of the payment (in BRL) -> {@code double}
+   */
   public void setAmount(double amount) {
     this.amount = amount;
   }
 
+  /**
+   * {@return the issueDate as a {@link LocalDateTime}}
+   */
   public LocalDateTime getIssueDate() {
     return issueDate;
   }
 
+  /**
+   * Setter for the issueDate attribute
+   * @param issueDate The issue date of the payment -> {@link LocalDateTime}
+   */
   public void setIssueDate(LocalDateTime issueDate) {
     this.issueDate = issueDate;
   }
 
+  /**
+   * {@return the dueDate as a {@link LocalDateTime}}
+   */
   public LocalDateTime getDueDate() {
     return dueDate;
   }
 
+  /**
+   * Setter for the dueDate attribute
+   * @param dueDate The due date of the payment -> {@link LocalDateTime}
+   */
   public void setDueDate(LocalDateTime dueDate) {
     this.dueDate = dueDate;
   }
 
+  /**
+   * {@return the paymentDate as a {@link LocalDateTime}}
+   */
   public LocalDateTime getPaymentDate() {
     return paymentDate;
   }
 
+  /**
+   * Setter for the paymentDate attribute
+   * @param paymentDate The payment date of the payment -> {@link LocalDateTime}
+   */
   public void setPaymentDate(LocalDateTime paymentDate) {
     this.paymentDate = paymentDate;
   }
 
+  /**
+   * {@return the paymentStatus as a {@link PaymentStatus}}
+   */
   public PaymentStatus getPaymentStatus() {
     return paymentStatus;
   }
 
+  /**
+   * Setter for the paymentStatus attribute
+   * @param paymentStatus The target PaymentStatus to set -> {@link PaymentStatus}
+   */
   public void setPaymentStatus(PaymentStatus paymentStatus) {
     this.paymentStatus = paymentStatus;
   }
 
+  /**
+   * {@return the sender as a {@link ECommerceUser}}
+   */
   public ECommerceUser getSender() {
     return sender;
   }
 
+  /**
+   * Setter for the sender attribute
+   * @param sender The sender of the payment -> {@link ECommerceUser}
+   */
   public void setSender(ECommerceUser sender) {
     this.sender = sender;
   }
 
+  /**
+   * {@return the receiver as a {@link ECommerceUser}}
+   */
   public ECommerceUser getReceiver() {
     return receiver;
   }
 
+  /**
+   * Setter for the receiver attribute
+   * @param receiver The receiver of the payment -> {@link ECommerceUser}
+   */
   public void setReceiver(ECommerceUser receiver) {
     this.receiver = receiver;
   }
