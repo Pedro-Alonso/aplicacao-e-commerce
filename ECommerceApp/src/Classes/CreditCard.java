b4d7747e4644b139e90638538e16715c2e077b67
matheusvirissimo <matheus.virissimo@unesp.br>
2024-09-08 23:10:42 -0300

    feat: new verifyLimit() funcion add, better verifications and minor fixs

--- ECommerceApp/src/Classes/CreditCard.java
+++ ECommerceApp/src/Classes/CreditCard.java
@@ -11,31 +11,72 @@ public class CreditCard extends PaymentCard{
         this.limiteCartao = limiteCartao;
     }
 
+    // verificar se as quantidades de parcelas são válidas
+    public boolean verificaQuantidadeParcela(int quantidadeParcelas){
+        if(quantidadeParcelas <= 0 || quantidadeParcelas > 12){
+            return false; // não é possível
+        }
+
+        return true;
+    }
+
     // Verificar limite do cartão
     public boolean verificarLimite(double limiteCartao){
-        if(limiteCartao <= 0){
-            return false; // cartão não possui limite disponível
+        if(limiteCartao <= 0 || super.getAmount() > limiteCartao){
+            return false; // cartão não possui limite disponível ou o valor do produto é maior que o limite disponível
         }
         
         return true; // cartão possui limite
     }
 
     // Alterar o limitie do cartão
-    public double alterarLimite(double limiteCartao, double valor){
+    public double alterarLimite(double limiteCartao){
         if(limiteCartao < 0){ // se o limite do cartão for menor que zero, não é possível deixar ele mais negativo
             return limiteCartao;
         }else{
-            limiteCartao -= valor;
+            limiteCartao -= super.getAmount();
         }
         
         return limiteCartao;
     }
 
     // Juros proveniente da taxa do banco
-    public double jurosBanco(double valorProduto){
-        return (valorProduto*0.02); // juros do banco sbore a transação de crédito de 3%
+    public double jurosBanco(){
+        return (super.getAmount()*0.02); // juros do banco sbore a transação de crédito de 2%
     }
 
+    // possibilidade de parcelamento
+    public double valorParcelamento(int quantidadeParcelas){
+        if(verificarLimite(limiteCartao) == false || verificaQuantidadeParcela(quantidadeParcelas) == false){
+            return 0; // cartão não possui limite disponível ou as parcelas são inválidas
+        }
+        
+        // uma só parcela não possui juros
+        if(quantidadeParcelas == 1){
+            return super.getAmount();
+        }
+
+        double valorParcela = (super.getAmount()/quantidadeParcelas)*jurosBanco(); // o valor total já está no super
+        
+        return valorParcela;
+        
+    }
+
+    public void exibirParcelas(int quantidadeParcelas){
+
+        if(valorParcelamento(quantidadeParcelas) == 0){
+            return;
+        }
+        
+        for(int i = 0; i < quantidadeParcelas; i++){
+            System.out.println("Data inicial: " + super.getPaymentDate().plusMonths(i+1));
+            System.out.println("Parcela: " + (i+1));
+            System.out.println("Valor parcela: R$" + valorParcelamento(quantidadeParcelas));
+            System.out.println("======================");
+        }
+
+        return;
+    }
 
 
     // Getters and setters
